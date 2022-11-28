231078beaf88578a4ae7ceed83a5bec1907a7104
insh007 <aliinshad9546@gmail.com>
2022-11-28 22:34:08 +0530

    phirse

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,21 @@ const router = express.Router()
 const userController = require("../controllers/userController");
 const bookController = require("../controllers/bookController")
 
+/*------------------------Create User-----------------------------------*/
 router.post("/register", userController.createUser);
+
+/*------------------------User Login-----------------------------------*/
 router.post("/login", userController.login);
+
+/*------------------------Fetch Books-----------------------------------*/
 router.get("/books", bookController.allBooks);
+
+/*---------------------------Book Create --------------------------------*/
 router.post("/createbook" , bookController.createBook)
 
-
-console.log("1")
+/*---------------------------Hit On Wrong Url --------------------------------*/
+router.all("/*", function(req, res){
+    return res.status(404).send({status:false, message : "Provided route url is wrong"})
+})
 
 module.exports = router
