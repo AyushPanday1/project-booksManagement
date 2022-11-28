62e53714c4f51365e1dcba1b7294abddc81d5404
AyushPanday1 <ayush7426883261@gmail.com>
2022-11-28 16:01:58 +0530

    DONE

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,4 +29,19 @@ const createUser = async function(req,res){
         /*------------------------Checking mail is unique or not-----------------------------------*/
         const uniqueEmail = await userModel.find({email:email})
         if(!uniqueEmail){return res.status(400).send({status : false, message : "email is already exit"})} 
-f
+
+        /*-------------------------------  Validation(Regex)  -----------------------------------*/
+        if(!(isValidTitle(title))){return res.status(400).send({status : false, msg : "Please enter correct title ex: Mr, Mrs, Miss"})}
+        if(!(isValidPhone(phone))){return res.status(400).send({status : false, msg : "Invalid phone number"})}
+        if(!(isValidMail(email))){return res.status(400).send({status : false, msg : "Invalid email Id"})}
+        if(!(isValidFullName(name))){return res.status(400).send({status : false, msg : "Please provide valid full name"})}
+        if(!(isValidPassword(password))){return res.status(400).send({status : false, msg : "Invalid password"})}
+        const createUser = await userModel.create(data)
+        return res.status(201).send({stauts:true, message:"Success", data:createUser})
+    }
+    catch(err){
+        return res.status(500).send({status:false, message:err.message})
+    }
+}
+
+module.exports.createUser = createUser
